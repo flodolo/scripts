079a22b0c690519f06608631cacfc6006565ea7e
Francesco Lodolo <francesco.lodolo@gmail.com>
2013-07-25 15:51:28 +0200

    Add HTML output, ignore dbg locale and round percentage to 1 decimal

--- marketplace/mpstats.py
+++ marketplace/mpstats.py
@@ -11,11 +11,91 @@ from optparse import OptionParser
 # git clone https://github.com/mozilla/webpay
 # git clone https://github.com/mozilla/zamboni
 
-path = "/home/flodolo/pei"
+def write_html(json, products, filename):
+    html_code = '''
+        <!DOCTYPE html>
+        <html lang="en-US">
+        <head>
+          <meta charset=utf-8>
+          <title>Repo Status</title>
+          <style type="text/css">
+            body {background-color: #f9f9f9; font-family: Arial, Verdana; font-size: 14px; padding: 10px 30px;}
+            p {margin-top: 2px;}
+            table {padding: 0; margin: 0; border-collapse: collapse; color: #333; background: #F3F5F7;}
+            table a {color: #3A4856; text-decoration: none; border-bottom: 1px solid #C6C8CB;}
+            table a:visited {color: #777;}
+            table a:hover {color: #000;}
+            table thead th {background: #3A4856; padding: 15px 10px; color: #fff; text-align: center; font-weight: normal; vertical-align: top;}
+            table .firstsection {padding-left: 40px; border-left: 1px solid #555;}
+            table .lastsection {padding-right: 40px; border-right: 1px solid #555;}
+            table tbody, table thead {border-left: 1px solid #EAECEE; border-right: 1px solid #EAECEE;}
+            table tbody {border-bottom: 1px solid #EAECEE;}
+            table tbody td, table tbody th {padding: 5px 20px; text-align: left;}
+            table tbody tr {background: #F5F5F5;}
+            table tbody tr.odd {background: #F0F2F4;}
+            table tbody tr:hover {background: #EAECEE; color: #111;}
+            table tbody tr.fixed {background: #A5FAB0;}
+            table tbody td.number {text-align: center;}
+            table tbody td.complete {background-color: #C2FCDD}
+            table tbody td.incomplete {background-color: #FF9191}
+          </style>
+        </head>
+        <body>
+            <table>
+              <thead>
+                <tr>
+                    <th>&nbsp;</th>
+                    <th colspan="4">Fireplace</th>
+                    <th colspan="4">Webpay</th>
+                    <th colspan="4">Zamboni</th>
+                <tr>
+                    <th>Locale</th>
+                    <th class="firstsection">Translated<br/>strings</th>
+                    <th>Untranslated<br/>strings</th>
+                    <th>Fuzzy<br/>strings</th>
+                    <th class="lastsection">%</th>
+                    <th class="firstsection">Translated<br/>strings</th>
+                    <th>Untranslated<br/>strings</th>
+                    <th>Fuzzy<br/>strings</th>
+                    <th class="lastsection">%</th>
+                    <th class="firstsection">Translated<br/>strings</th>
+                    <th>Untranslated<br/>strings</th>
+                    <th>Fuzzy<br/>strings</th>
+                    <th class="lastsection">%</th>
+                </tr>
+              </thead>
+              <tbody>
+        '''
+
+    for locale in sorted(json):
+        html_code = html_code + "<tr>\n<td>" + locale + "</td>"
+        for product in products:
+            try:
+                html_code = html_code + "  <td class='firstsection'>" + str(json[locale][product]['translated']) + "</td>\n"
+                html_code = html_code + "  <td>" + str(json[locale][product]['untranslated']) + "</td>\n"
+                html_code = html_code + "  <td>" + str(json[locale][product]['fuzzy']) + "</td>\n"
+                if (json[locale][product]['complete']):
+                    css_class="class='lastsection complete'"
+                else:
+                    css_class="class='lastsection incomplete'"
+                html_code = html_code + "  <td " + css_class + ">" + str(json[locale][product]['percentage']) + "</td>\n"
+            except Exception as e:
+                html_code = html_code + '''  <td class='firstsection'>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td class='lastsection'>&nbsp;</td>
+                '''
+                print e
+
+
+        html_code = html_code + "</tr>"
+
+    json_file = open(filename, "w")
+    json_file.write(html_code)
+    json_file.close()
+
 
 def main():
-    global path
-
     # Parse command line options
     clparser = OptionParser()
     clparser.add_option("-o", "--output", help="Choose a type of output", choices=["html", "json"], default="json")
@@ -23,17 +103,18 @@ def main():
     (options, args) = clparser.parse_args()
     output_type = options.output
 
-    # List of products, and where I want to store the json file
+    path = "/home/flodolo/pei"
     products = ["fireplace", "webpay", "zamboni"]
     json_filename = "/home/flodolo/github/transvision/web/marketplace.json"
+    html_filename = "/home/flodolo/github/transvision/web/marketplace.html"
     json_data = {}
 
     for product in products:
         product_folder = path + "/" + product + "/locale"
 
         for locale in sorted(os.listdir(product_folder)):
-            # Ignore files, just folders, and ignore folder called "templates"
-            if (os.path.isdir(os.path.join(product_folder, locale))) & (locale != "templates"):
+            # Ignore files, just folders, and ignore folder called "templates" and "dbg"
+            if (os.path.isdir(os.path.join(product_folder, locale))) & (locale != "templates") & (locale != "dbg"):
                 print os.path.join(product_folder, locale)
 
                 cmd = "msgfmt --statistics " + os.path.join(product_folder, locale) + "/LC_MESSAGES/messages.po"
@@ -76,7 +157,7 @@ def main():
                 else:
                     # Need to calculate the completeness
                     complete = False
-                    percentage = round((float(string_translated) / string_total) * 100, 2)
+                    percentage = round((float(string_translated) / string_total) * 100, 1)
 
                 status_record = {
                     "total": string_total,
@@ -100,6 +181,9 @@ def main():
         json_file.write(json.dumps(json_data, indent=4, sort_keys=True))
         json_file.close()
 
+    # Write back updated json data
+    if (output_type == 'html'):
+        write_html(json_data, products, html_filename)
 
 if __name__ == "__main__":
     main()
