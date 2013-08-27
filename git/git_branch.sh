17081540fc8d063dee3b8c9414e2eb2437758d7a
Francesco Lodolo <flod@lodolo.net>
2013-08-27 12:17:29 +0200

    Update git branch script
    Checkout master and update before branching

--- git/git_branch.sh
+++ git/git_branch.sh
@@ -8,6 +8,13 @@ if [ $# -lt 2 ]
 fi
 
 cd $1
+# Checkout master and update
+git checkout master
+git fetch upstream
+git merge upstream/master
+git pull --recurse-submodules && git submodule update
+
+# Branch
 git branch $2
 git checkout $2
 git push origin $2
