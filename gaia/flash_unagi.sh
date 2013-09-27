371c4c47934296dc6fd9ee0037c09d06f1db61d4
Francesco Lodolo <flod@lodolo.net>
2013-09-27 19:49:25 +0200

    Flash Unagi
    Added -no-update param to use local changes

--- gaia/flash_unagi.sh
+++ gaia/flash_unagi.sh
@@ -1,20 +1,36 @@
 #! /usr/bin/env bash
-# Link https://l10n.etherpad.mozilla.org/gaia-multilocale
+
+# Reference Link: https://l10n.etherpad.mozilla.org/gaia-multilocale
 
 localecode="it"
 
-# This line force reads $PATH from .profile on OS X
-# Ref: http://www.tech-recipes.com/rx/2621/os_x_change_path_environment_variable/
+updatelocale=true
+
+# One parameter from command line
+if [ $# -eq 1 ] && [ $1 == '-no-update' ]
+then
+	updatelocale=false
+fi
+
 cd ~
 . ./.profile echo $PATH
 
-# Pull Gaia, pull locale and make
+echo ""
+echo "-----------------------"
+echo "Updating Gaia"
 cd ~/moz/gaia/
 git checkout v1-train
 git reset --hard
 git pull
-cd locales/$localecode
-hg pull -u
-cd ../..
+
+if $updatelocale
+then
+	echo ""
+	echo "-----------------------"
+	echo "Updating locale"
+	cd locales/$localecode
+	hg pull -u
+	cd ../..
+fi
 
 make clean && PRODUCTION=1 make install-gaia MAKECMDGOALS=production MOZILLA_OFFICIAL=1 GAIA_DEFAULT_LOCALE=$localecode LOCALES_FILE=locales/languages_all.json LOCALE_BASEDIR=locales/ REMOTE_DEBUGGER=1
