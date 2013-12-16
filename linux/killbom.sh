d1f3a80822c370139c3c1a5a3e7d2743bd7f257c
Francesco Lodolo (:flod) <flod@lodolo.net>
2013-12-16 14:00:20 +0100

    killbom: check only .lang and .txt files

--- linux/killbom.sh
+++ linux/killbom.sh
@@ -5,7 +5,7 @@ grep -rn $'\xEF\xBB\xBF' .
 echo '----------------------'
 
 echo 'Cleaning up...'
-find . -type f -exec sed -i '1 s/^\xef\xbb\xbf//' {} +
+find . -type f \(-iname "*.lang" -or -iname "*.txt" \) -exec sed -i '1 s/^\xef\xbb\xbf//' {} +
 
 echo '----------------------'
 echo 'Checking again. Files with BOM:'
