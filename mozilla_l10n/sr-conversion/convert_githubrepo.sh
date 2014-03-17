6a31b91c646fdc55c145db7b9a48cf0ff3684bed
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-03-17 07:50:00 +0100

    sr web conversion: fixed script (remove /, git add correct files)

--- mozilla_l10n/sr-conversion/convert_githubrepo.sh
+++ mozilla_l10n/sr-conversion/convert_githubrepo.sh
@@ -8,8 +8,15 @@ then
 fi
 
 current_date=$(date +"%Y%m%d")
+product_name="$1"
 
-cd $1
+if [ "${product_name: -1}" == "/" ]
+then
+	# Remove last character "/"
+	product_name="${product_name%?}"
+fi
+
+cd $product_name
 # Checkout master and update
 git checkout master
 git fetch upstream
@@ -23,18 +30,21 @@ git push origin $current_date
 
 # Convert
 echo "Converting strings"
-if [ "$1" == "fxa-content-server-l10n" ]
+if [ "$product_name" == "fxa-content-server-l10n" ]
 then
 	/usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o locale/sr_Latn/LC_MESSAGES/client.po locale/sr/LC_MESSAGES/client.po
 	/usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o locale/sr_Latn/LC_MESSAGES/server.po locale/sr/LC_MESSAGES/server.po
-elif [ "$1" == "zippy" ]
+	git add locale/sr_Latn/LC_MESSAGES/client.po
+	git add locale/sr_Latn/LC_MESSAGES/server.po
+elif [ "$product_name" == "zippy" ]
 then
 	/usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o locale/sr_LATN/LC_MESSAGES/messages.po locale/sr/LC_MESSAGES/messages.po
+	git add locale/sr_Latn/LC_MESSAGES/messages.po
 else
 	/usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o locale/sr_Latn/LC_MESSAGES/messages.po locale/sr/LC_MESSAGES/messages.po
+	git add locale/sr_Latn/LC_MESSAGES/messages.po
 fi
 
 # Add change to git
-git add locale/sr_Latn/LC_MESSAGES/messages.po
 git commit -a -m "Script conversion from Serbian Cyrillic to Latin ($current_date)"
 git push -u origin $current_date
