7aee91b4b58e13369e8d7dd27a3a5aede2d2d06b
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-03-21 17:52:59 +0100

    mpstats: reduce font and paddings, table too large

--- marketplace/mpstats.py
+++ marketplace/mpstats.py
@@ -17,18 +17,18 @@ def write_html(json, products, filename):
           <style type="text/css">
             body {background-color: #FFF; font-family: Arial, Verdana; font-size: 14px; padding: 10px 30px;}
             p {margin-top: 2px;}
-            table {padding: 0; margin: 0; border-collapse: collapse; color: #333; background: #F3F5F7;}
-            table a {color: #3A4856; text-decoration: none; border-bottom: 1px solid #DDD;}
+            table {padding: 0; margin: 0; border-collapse: collapse; color: #333; background: #F3F5F7; font-size: 12px;}
+            table a {color: #3A4856; text-decoration: none; border-bottom: 1px solid #DDD; }
             table a:visited {color: #777;}
             table a:hover {color: #000;}
-            table thead th {background: #EAECEE; padding: 15px 10px; color: #000; text-align: center; font-weight: bold; vertical-align: top;}
+            table thead th {background: #EAECEE; padding: 15px 5px; color: #000; text-align: center; font-weight: bold; vertical-align: top;}
             table tr th {background: #EAECEE;}
             table td.firstsection {padding-left: 40px; border-left: 1px solid #DDD;}
             table td.lastsection {padding-right: 40px; border-right: 1px solid #DDD;}
             table .lastsection:last-child {border-right: 0};
             table tbody, table thead {border-left: 1px solid #DDD; border-right: 1px solid #DDD;}
             table tbody {border-bottom: 1px solid #DDD;}
-            table tbody td, table tbody th {padding: 5px 10px; text-align: left;}
+            table tbody td, table tbody th {padding: 5px; text-align: left;}
             table tbody td {border-bottom: 1px solid #DDD}
             table tbody tr {background: #F5F5F5;}
             table tbody tr.odd {background: #F0F2F4;}
