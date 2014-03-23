0d8940657c9dd536fb7f18f1b1ad16d6bab32756
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-03-23 12:39:21 +0100

    sr-conversion: fix failure when file is remove from sr-Cyrl

--- mozilla_l10n/sr-conversion/conversion_in.py
+++ mozilla_l10n/sr-conversion/conversion_in.py
@@ -19,20 +19,26 @@ for rev in revs:
     files = subprocess.check_output(hg_cyrl + ['log', '-r', '.', '--template','{files}']).split()
     desc = subprocess.check_output(hg_cyrl + ['log', '-r', '.', '--template','{desc}'])
     for f in files:
-        # If the same file doesn't exist in sr-Latn, script fails
         file_latn = 'sr-Latn/' + f
         file_cyrl = 'sr-Cyrl/' + f
         if not os.path.isfile(file_latn):
+            # If the same file doesn't exist in sr-Latn, script fails
             path_latn = os.path.dirname(file_latn)
             if not os.path.exists(path_latn):
                 # Create folder too since is missing
                 os.makedirs(path_latn)
                 print "Creating missing folders: " + path_latn
             open(file_latn, 'a').close()
-
             print 'Adding file to repo: ' + file_latn
             subprocess.check_call(hg_latn + ['add', file_latn])
-
+        if not os.path.isfile(file_cyrl):
+            # File has been deleted from sr-Cyrl, can't convert this one
+            # File has been deleted from sr-Cyrl, can't convert this one
+            print 'Removing file from repo: ' + file_latn
+            subprocess.check_call(hg_latn + ['remove', file_latn])
+            continue
         subprocess.check_call([uconv, '-x', 'Serbian-Latin/BGN',
                                '-o', file_latn, file_cyrl])
+
+    # Commit changeset
     subprocess.check_call(hg_latn + ['ci', '-m', desc, '-u', author])
