1cbc40614cd30387530ebf99ef1a22339a6d9207
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-03-25 16:29:43 +0100

    sr conversion: don't die on empty hg in

--- mozilla_l10n/sr-conversion/conversion_in.py
+++ mozilla_l10n/sr-conversion/conversion_in.py
@@ -5,40 +5,46 @@
 import os
 import subprocess
 
+revs = []
 hg_cyrl = ['hg', '-R', 'sr-Cyrl']
 hg_latn = ['hg', '-R', 'sr-Latn']
 uconv = '/usr/local/opt/icu4c/bin/uconv'
 
-revs = subprocess.check_output(hg_cyrl + ['-q', 'in',
-                               '--template', '{rev}\n']).split()
-for rev in revs:
-    print rev
-    subprocess.check_call(hg_cyrl + ['pull', '-u', 'default'])
-    subprocess.check_call(hg_cyrl + ['update', '-r', rev])
-    author = subprocess.check_output(hg_cyrl + ['log', '-r', '.', '--template','{author}'])
-    files = subprocess.check_output(hg_cyrl + ['log', '-r', '.', '--template','{files}']).split()
-    desc = subprocess.check_output(hg_cyrl + ['log', '-r', '.', '--template','{desc}'])
-    for f in files:
-        file_latn = 'sr-Latn/' + f
-        file_cyrl = 'sr-Cyrl/' + f
-        if not os.path.isfile(file_latn):
-            # If the same file doesn't exist in sr-Latn, script fails
-            path_latn = os.path.dirname(file_latn)
-            if not os.path.exists(path_latn):
-                # Create folder too since is missing
-                os.makedirs(path_latn)
-                print "Creating missing folders: " + path_latn
-            open(file_latn, 'a').close()
-            print 'Adding file to repo: ' + file_latn
-            subprocess.check_call(hg_latn + ['add', file_latn])
-        if not os.path.isfile(file_cyrl):
-            # File has been deleted from sr-Cyrl, can't convert this one
-            # File has been deleted from sr-Cyrl, can't convert this one
-            print 'Removing file from repo: ' + file_latn
-            subprocess.check_call(hg_latn + ['remove', file_latn])
-            continue
-        subprocess.check_call([uconv, '-x', 'Serbian-Latin/BGN',
-                               '-o', file_latn, file_cyrl])
+try:
+    revs = subprocess.check_output(hg_cyrl + ['-q', 'in',
+                                   '--template', '{rev}\n']).split()
+except Exception as e:
+    print "No incoming changesets"
 
-    # Commit changeset
-    subprocess.check_call(hg_latn + ['ci', '-m', desc, '-u', author])
+if revs:
+    for rev in revs:
+        print rev
+        subprocess.check_call(hg_cyrl + ['pull', '-u', 'default'])
+        subprocess.check_call(hg_cyrl + ['update', '-r', rev])
+        author = subprocess.check_output(hg_cyrl + ['log', '-r', '.', '--template','{author}'])
+        files = subprocess.check_output(hg_cyrl + ['log', '-r', '.', '--template','{files}']).split()
+        desc = subprocess.check_output(hg_cyrl + ['log', '-r', '.', '--template','{desc}'])
+        for f in files:
+            file_latn = 'sr-Latn/' + f
+            file_cyrl = 'sr-Cyrl/' + f
+            if not os.path.isfile(file_latn):
+                # If the same file doesn't exist in sr-Latn, script fails
+                path_latn = os.path.dirname(file_latn)
+                if not os.path.exists(path_latn):
+                    # Create folder too since is missing
+                    os.makedirs(path_latn)
+                    print "Creating missing folders: " + path_latn
+                open(file_latn, 'a').close()
+                print 'Adding file to repo: ' + file_latn
+                subprocess.check_call(hg_latn + ['add', file_latn])
+            if not os.path.isfile(file_cyrl):
+                # File has been deleted from sr-Cyrl, can't convert this one
+                # File has been deleted from sr-Cyrl, can't convert this one
+                print 'Removing file from repo: ' + file_latn
+                subprocess.check_call(hg_latn + ['remove', file_latn])
+                continue
+            subprocess.check_call([uconv, '-x', 'Serbian-Latin/BGN',
+                                   '-o', file_latn, file_cyrl])
+
+        # Commit changeset
+        subprocess.check_call(hg_latn + ['ci', '-m', desc, '-u', author])
