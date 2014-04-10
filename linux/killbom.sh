7d8d6bf846918ec61daa4c45f5fe579f9222f2c4
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-04-10 16:51:39 +0200

    Update killbom

--- linux/killbom.sh
+++ linux/killbom.sh
@@ -1,12 +1,11 @@
 #! /usr/bin/env bash
 
 echo 'Files with BOM:'
-grep -rn $'\xEF\xBB\xBF' .
-echo '----------------------'
+find . -type f -iname '*.lang' -print0 | xargs -0r awk '/^\xEF\xBB\xBF/ {print FILENAME} {nextfile}'
 
-echo 'Cleaning up...'
-find . -type f \(-iname "*.lang" -or -iname "*.txt" \) -exec sed -i '1 s/^\xef\xbb\xbf//' {} +
+echo 'Cleaning up:'
+find . -type f -iname '*.lang' -exec sed -i -e '1s/^\xEF\xBB\xBF//' {} \;
 
 echo '----------------------'
 echo 'Checking again. Files with BOM:'
-grep -rn $'\xEF\xBB\xBF' .
+find . -type f -iname '*.lang' -print0 | xargs -0r awk '/^\xEF\xBB\xBF/ {print FILENAME} {nextfile}'
