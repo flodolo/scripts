a0385709e8deee144ba7a0acf7f95214fd67c8d8
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-05-07 13:36:10 +0200

    View for bug 994248 (check SSL + Yahoo)

---
+++ productization/webviews/bug994248_ssl/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset=utf-8>
+    <title>Check SSL on handlers + Yahoo</title>
+    <style type="text/css">
+        body { background-color: #FCFCFC; font-family: Arial, Verdana; font-size: 14px; padding: 10px; }
+        p { margin-top: 2px; }
+        h2 { clear: both; }
+        .green { color: green; }
+        .red { color: red; }
+    </style>
+</head>
+
+<body>
+
+<?php
+    include_once('../locales.inc');
+    $filename = '../searchplugins.json';
+
+    $jsondata = file_get_contents($filename);
+    $jsonarray = json_decode($jsondata, true);
+
+    $channel = 'aurora';
+    $products = array('browser', 'mobile');
+    $productnames = array('Firefox Desktop', 'Firefox Mobile (Android)');
+    $html_output = '';
+
+    foreach ($products as $i=>$product) {
+        $html_output .= "<h1>{$product}</h1>";
+        $locale_list = '';
+        $nonen_locale_list = '';
+        foreach ($locales as $locale) {
+            $html_output .= "<h2>{$locale}</h2>";
+            if (array_key_exists($product, $jsonarray[$locale])) {
+                if (array_key_exists($channel, $jsonarray[$locale][$product])) {
+                    // I have searchplugins for this locale
+                    foreach ($jsonarray[$locale][$product][$channel] as $key => $singlesp) {
+                        if ($key != 'p12n') {
+                            $spfilename = strtolower($singlesp['file']);
+                            if (strpos($spfilename, 'yahoo') !== false) {
+                                $locale_list .= "{$locale}, ";
+                                if (strpos($singlesp['description'], 'en-US') === false) {
+                                    $nonen_locale_list .= "{$locale}, ";
+                                }
+                                if (array_key_exists(2, $jsonarray[$locale][$product][$channel]['p12n']['searchorder'])) {
+                                    $second_search = strtolower($jsonarray[$locale][$product][$channel]['p12n']['searchorder'][2]);
+                                    if (strpos($second_search, 'yahoo') === false) {
+                                        $html_output .= "<p>{$locale} doesn't have Yahoo as second ({$second_search})</p>";
+                                    }
+                                } else {
+                                   $html_output .= "<p>{$locale} doesn't have a second search plugin</p>";
+                                }
+                            }
+
+                            if (strpos($singlesp['url'], 'yahoo') !== false ||
+                                strpos($singlesp['url'], 'google') !== false) {
+                                if (! $singlesp['secure']) {
+                                    $html_output .= "<p>{$singlesp['file']}: ";
+                                    $html_output .= "<span class='red'>not SSL</span></p>";
+                                }
+                            }
+                        } else {
+                            // Check productization for Google and Yahoo
+                            if (array_key_exists('mailto', $singlesp['contenthandlers'])) {
+                                $contenthandlers = $singlesp['contenthandlers']['mailto'];
+                                foreach ($contenthandlers as $contenthandler) {
+                                    if (strpos($contenthandler['uri'], 'yahoo') !== false ||
+                                        strpos($contenthandler['uri'], 'google') !== false ||
+                                        strpos($contenthandler['uri'], '30boxes') !== false) {
+                                        $html_output .= "<p>{$contenthandler['name']} (mailto): ";
+                                        if ((strpos($contenthandler['uri'], 'https') === false)) {
+                                            $html_output .= "<span class='red'>not SSL</span></p>";
+                                        } else {
+                                            $html_output .= "<span class='green'>SSL</span></p>";
+                                        }
+                                    }
+                                }
+                            } else {
+                                $html_output .= "<p class='red'>mailto handler is missing</p>";
+                            }
+
+                            if (array_key_exists('feedhandlers', $singlesp)) {
+                                $feedhandlers = $singlesp['feedhandlers'];
+                                foreach ($feedhandlers as $feedhandler) {
+                                    if (strpos($feedhandler['uri'], 'yahoo') !== false ||
+                                        strpos($feedhandler['uri'], 'google') !== false) {
+                                        $html_output .= "<p>{$feedhandler['title']} (content): ";
+                                        if ((strpos($feedhandler['uri'], 'https') === false)) {
+                                            $html_output .= "<span class='red'>not SSL</span></p>";
+                                        } else {
+                                            $html_output .= "<span class='green'>SSL</span></p>";
+                                        }
+                                    }
+                                }
+                            } else {
+                                $html_output .= "<p class='red'>feed handler is missing</p>";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        $html_output .= "<p>List of locales with Yahoo: {$locale_list}</p>";
+        $html_output .= "<p>List of locales with localized versions of Yahoo: {$nonen_locale_list}</p>";
+    }
+
+    echo $html_output;
