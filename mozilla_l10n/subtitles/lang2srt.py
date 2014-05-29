8ac88383c2ff8dcd89be49a25c703007d4fc45f0
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-05-29 15:30:02 +0200

    Python script to convert srt <-> lang format

---
+++ mozilla_l10n/subtitles/lang2srt.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import json
+import os
+import sys
+
+
+def readlang(filename):
+	try:
+		json_subtitles = {}
+		langfile = open(filename)
+		current_index = timing = subtitle = "";
+		for line in langfile:
+			line = line.strip()
+			if line != "":
+				if line.startswith('#') and current_index == "":
+					# Remove comment symbol and extra space, get index and timing
+					line = line.lstrip('#').strip()
+					current_index, timing = line.split("=>")
+					current_index = int(current_index)
+				else:
+					# Ignore original starting with ;
+					if not line.startswith(";"):
+						# Read subtitle, replace <br> with \n
+						subtitle = line.replace("<br>", "\n")
+						# Store in json
+						json_subtitles[current_index] = {
+							"timing": timing.strip(),
+							"subtitle": subtitle.strip()
+						}
+						current_index = ""
+
+		# Debug
+		# jsonfile = open("output.txt", "w")
+		# jsonfile.write(json.dumps(json_subtitles, indent=4, sort_keys=True))
+		# jsonfile.close()
+
+		langfile.close();
+		return json_subtitles
+	except Exception as e:
+		print e
+
+
+def writesrt(subtitles, filename):
+	try:
+		srtfile = open(filename, "w")
+		for index, subtitle in subtitles.iteritems():
+			srtfile.write(str(index) + "\n")
+			srtfile.write(subtitle["timing"] + "\n")
+			srtfile.write(subtitle["subtitle"] + "\n\n")
+		srtfile.close()
+	except Exception as e:
+		print e
+
+
+def main():
+	if len(sys.argv) != 2:
+		print "Error: missing filename.\nSyntax: lang2srt.py examplefile.lang"
+		sys.exit(0)
+
+	lang_filename = sys.argv[1]
+
+	if (os.path.splitext(lang_filename)[1] == ".lang"):
+		srt_filename = os.path.splitext(lang_filename)[0] + ".srt"
+	else:
+		srt_filename = lang_filename + ".srt"
+
+	print "\nExtracting data from " + lang_filename
+	json_subtitles = readlang(lang_filename)
+	print "\nSaving data in " + srt_filename
+	writesrt(json_subtitles, srt_filename)
+
+
+if __name__ == "__main__":
+    main()
---
+++ mozilla_l10n/subtitles/srt2lang.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import json
+import os
+import sys
+
+
+def readsrt(filename):
+	try:
+		json_subtitles = {}
+		srtfile = open(filename)
+		current_index = timing = subtitle = "";
+		for line in srtfile:
+			line = line.strip()
+			if line != "":
+				if current_index == "":
+					# Start of this subtitle
+					try:
+						current_index = int(line)
+					except Exception as e:
+						print e
+				else:
+					if timing == "":
+						# Timing information
+						timing = line
+					else:
+						if subtitle == "":
+							# Start of actual subtitle
+							subtitle = line
+						else:
+							# Multiline subtitle
+							subtitle = subtitle + "<br>" + line
+			else:
+				# End of this subtitle
+				json_subtitles[current_index] = {
+					"timing": timing,
+					"subtitle": subtitle
+				}
+				current_index = timing = subtitle = "";
+
+		# Reached end of file, save the last subtitle
+		json_subtitles[current_index] = {
+			"timing": timing,
+			"subtitle": subtitle
+		}
+
+		# Debug
+		# jsonfile = open("output.txt", "w")
+		# jsonfile.write(json.dumps(json_subtitles, indent=4, sort_keys=True))
+		# jsonfile.close()
+
+		srtfile.close();
+		return json_subtitles
+	except Exception as e:
+		print e
+
+
+def writelang(subtitles, filename):
+	try:
+		langfile = open(filename, "w")
+		for index, subtitle in subtitles.iteritems():
+			langfile.write("# " + str(index) + "=>" + subtitle["timing"] + "\n")
+			langfile.write(";" + subtitle["subtitle"] + "\n")
+			langfile.write(subtitle["subtitle"] + "\n\n\n")
+		langfile.close()
+	except Exception as e:
+		print e
+
+
+def main():
+	if len(sys.argv) != 2:
+		print "Error: missing filename.\nSyntax: srt2lang.py examplefile.srt"
+		sys.exit(0)
+
+	srt_filename = sys.argv[1]
+
+	if (os.path.splitext(srt_filename)[1] == ".srt"):
+		lang_filename = os.path.splitext(srt_filename)[0] + ".lang"
+	else:
+		lang_filename = srt_filename + ".lang"
+
+	print "\nExtracting data from " + srt_filename
+	json_subtitles = readsrt(srt_filename)
+	print "\nSaving data in " + lang_filename
+	writelang(json_subtitles, lang_filename)
+
+
+if __name__ == "__main__":
+    main()
