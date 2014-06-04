bc5bd6905e6e85926953d37d1f6ec62605ebda4f
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-06-04 10:51:51 +0200

    Upload to amara: refactor

--- mozilla_l10n/subtitles/uploadtoamara.py
+++ mozilla_l10n/subtitles/uploadtoamara.py
@@ -10,8 +10,8 @@ the script to work. This information can be saved in a file called auth.txt,
 using JSON format
 
 {
-	"amara_apikey": "API_ID",
-	"amara_username": "USERNAME"
+	"amara_apikey"   : "API_ID",
+	"amara_username" : "USERNAME"
 }
 
 See also http://amara.readthedocs.org/en/latest/api.html
@@ -23,54 +23,76 @@ import sys
 import urllib2
 
 
-amara_apikey = ""
-amara_username = ""
+# Add your amara auth data
+authdata = {
+	"username" : "",
+	"apikey"   : ""
+}
+
+
+def getRequest(url, authdata):
+	headers = {
+		"Accept"         : "application/json",
+		"X-api-username" : authdata["username"],
+		"X-apikey"       : authdata["apikey"]
+	}
+	req = urllib2.Request(url, None, headers)
+	try:
+		response = urllib2.urlopen(req)
+		return {
+			"success" : True,
+			"result"  : response.read()
+		}
+	except Exception as e:
+		return {
+			"success" : False,
+			"result"  : e.code
+		}
 
 
 def postRequest(url, values, authdata):
 	headers = {
-		"Content-Type": "application/json",
-		"X-api-username": authdata["username"],
-		"X-apikey": authdata["apikey"]
+		"Content-Type"   : "application/json",
+		"X-api-username" : authdata["username"],
+		"X-apikey"       : authdata["apikey"]
 	}
 	req = urllib2.Request(url, json.dumps(values), headers)
 	try:
 		response = urllib2.urlopen(req)
-		print response.read()
+		return {
+			"success" : True,
+			"result"  : response.read()
+		}
 	except Exception as e:
-		print e
-		sys.exit(0)
+		return {
+			"success" : False,
+			"result"  : e
+		}
 
 
 def main():
-	global amara_username
-	global amara_apikey
+	global authdata
 
-	if amara_username == "" or amara_apikey == "":
+	if authdata["username"] == "" or authdata["apikey"] == "":
 		# Search for auth.json, try to set variables from it
 		try:
 			jsonfile = open("auth.json")
 			api_data = json.load(jsonfile)
-			amara_username = api_data["amara_username"]
-			amara_apikey = api_data["amara_apikey"]
+			authdata["username"] = api_data["amara_username"]
+			authdata["apikey"]   = api_data["amara_apikey"]
 		except Exception as e:
 			print e
 
-	if amara_username == "" or amara_apikey == "":
+	if authdata["username"] == "" or authdata["apikey"] == "":
 		print "Error: missing mandatory parameters (Amara API key and username)"
 		sys.exit(0)
 
 	if len(sys.argv) != 4:
-		print "Error: missing parameters.\nSyntax: uploadtoamara.py videoid localeid srtfile"
+		print "Error: wrong number of parameters.\nSyntax: uploadtoamara.py videoid localeid srtfile"
 		sys.exit(0)
 
-	authdata = {
-		"username" : amara_username,
-		"apikey"   : amara_apikey
-	}
-
-	video_id = sys.argv[1]
-	locale_id = sys.argv[2]
+	video_id     = sys.argv[1]
+	locale_id    = sys.argv[2]
 	srt_filename = sys.argv[3]
 
 	# Read subtitles
@@ -82,33 +104,26 @@ def main():
 		sys.exit(0)
 
 	# Check if this language exists
-	missing_language = False
-	print "Checking if language exists..."
+	print "\nChecking if language exists..."
 	url = "https://www.amara.org/api2/partners/videos/" + video_id + "/languages/" + locale_id + "/"
-	headers = {
-		"Accept": "application/json",
-		"X-api-username": amara_username,
-		"X-apikey": amara_apikey
-	}
-	req = urllib2.Request(url, None, headers)
-	try:
-		response = urllib2.urlopen(req)
-		print response.read()
-	except Exception as e:
-		if e.code == 404:
-			missing_language = True
-		else:
-			print e
+	response = getRequest(url, authdata)
+	if not response["success"] and response["result"] == 404:
+		missing_language = True
+	else:
+		missing_language = False
 
-
-	# Missing locale, need to create it
+	# Missing language, need to create it
 	if missing_language:
 		print "Language is missing on Amara, creating..."
 		url = "https://www.amara.org/api2/partners/videos/" + video_id + "/languages/"
 		values = {
 			"language_code": locale_id
 		}
-		postRequest(url, values, authdata)
+		response = postRequest(url, values, authdata)
+		if not response["success"]:
+			print "There was an error"
+			print response["result"]
+			sys.exit(0)
 
 	# Uploading a file
 	print "Uploading file to Amara..."
@@ -117,7 +132,13 @@ def main():
 		"sub_format": "srt",
 		"subtitles": srtfile
 	}
-	postRequest(url, values, authdata)
+	response = postRequest(url, values, authdata)
+	if not response["success"]:
+		print "There was an error"
+		print response["result"]
+	else:
+		json_response = json.loads(response["result"])
+		print "File uploaded. Subtitle version: ", json_response["version_number"]
 
 
 if __name__ == "__main__":
