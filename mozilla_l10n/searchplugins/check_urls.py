8807cb977b161467796a4d73a6dd8b73c70fd9b7
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-09-18 09:21:00 +0200

    Check URLs: fix error with Error responses with utf-8 characters

--- mozilla_l10n/searchplugins/check_urls.py
+++ mozilla_l10n/searchplugins/check_urls.py
@@ -4,13 +4,13 @@ import json
 import urllib2
 
 def main():
-    json_file = open('searchplugins.json')
+    json_file = open("searchplugins.json")
     json_data = json.load(json_file)
 
     # Ignore metadata, images and en-US
-    del json_data['images']
-    del json_data['creation_date']
-    del json_data['en-US']
+    del json_data["images"]
+    del json_data["creation_date"]
+    del json_data["en-US"]
 
     locales = []
     for locale in json_data:
@@ -20,27 +20,29 @@ def main():
     output_results = []
     for locale in locales:
         output_results.append(locale)
-        for product in ['browser', 'mobile']:
+        for product in ["browser", "mobile"]:
             if product in json_data[locale]:
-                output_results.append('** %s **' % (product))
-                for element in json_data[locale][product]['aurora']:
-                    if (element != 'p12n' and
-                        'en-US' not in json_data[locale][product]['aurora'][element]['description']):
+                output_results.append("** %s **" % (product))
+                for element in json_data[locale][product]["aurora"]:
+                    if (element != "p12n" and
+                        "en-US" not in json_data[locale][product]["aurora"][element]["description"]):
                         # Ignore p12n and non localized searchplugins
-                        sp = json_data[locale][product]['aurora'][element]
-                        url_request = sp['url']
-                        print 'Checking %s %s' % (locale, sp['file'])
+                        sp = json_data[locale][product]["aurora"][element]
+                        url_request = sp["url"]
+                        print "Checking %s %s" % (locale, sp["file"])
                         try:
-                            response = urllib2.urlopen(url_request.encode('utf8'))
+                            response = urllib2.urlopen(url_request.encode("utf8"))
                             result = response.code
                         except urllib2.HTTPError, e:
-                            result = 'ERROR (%s)' % (e.code)
+                            result = "ERROR (%s)" % (e.code)
+                            result = unicode(result, "utf-8")
                         except urllib2.URLError, e:
-                            result = 'ERROR (%s)' % (e.args)
-                        output_results.append("%s: %s" % (sp['file'], result))
+                            result = "ERROR (%s)" % (e.args)
+                            result = unicode(result, "utf-8")
+                        output_results.append("%s: %s" % (sp["file"], result))
 
-    f = open('results.txt', "w")
-    f.write("\n".join(output_results).encode('utf8'))
+    f = open("results.txt", "w")
+    f.write("\n".join(output_results).encode("utf8"))
     f.close()
 
 
