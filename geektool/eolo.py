8a2803b64961ca818be971ad2aa556cd35bea874
Francesco Lodolo (:flod) <flod@lodolo.net>
2015-05-12 11:48:26 +0200

    Eolo: don't divide by zero on last and first day of the cycle

--- geektool/eolo.py
+++ geektool/eolo.py
@@ -11,8 +11,13 @@ try:
     if json_data['response']['status'] == 200:
         data = json_data['data']
         # Days before reset
+        cycle_length = 30
         end_date = datetime.strptime(data['nextReset'], "%Y-%m-%d %H:%M:%S")
-        remaining_days = abs((end_date - datetime.today()).days)
+        remaining_days = abs((end_date - datetime.today()).days);
+        if cycle_length - remaining_days > 0:
+            used_days = cycle_length - remaining_days
+        else:
+            used_days = 1
         # Data quota/usage
         quota_mb = data['quota'] / 1024
         used_mb = data['used'] / 1024
@@ -21,8 +26,11 @@ try:
         print "Rimamenti:        %5.0f MB" % remaining_mb
         print "Quota:            %5.0f MB" % quota_mb
         print "Giorni:              %2.0f g" % remaining_days
+        # Avoid division by 0
+        if remaining_days == 0:
+            remaining_days = 1;
         print "Media consumo:    %5.0f MB/g" % \
-              (used_mb / (30 - remaining_days))
+              (used_mb / used_days)
         print "Media dispon.:    %5.0f MB/g" % \
               (remaining_mb / remaining_days)
 except urllib2.HTTPError, err:
