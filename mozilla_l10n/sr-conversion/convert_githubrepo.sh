73d4428244c13f25ef4670b4371f636b7b8c4646
Francesco Lodolo (:flod) <flod@lodolo.net>
2015-05-26 17:49:53 +0200

    GitHub Serbian conversion: make script more portable

--- mozilla_l10n/sr-conversion/convert_githubrepo.sh
+++ mozilla_l10n/sr-conversion/convert_githubrepo.sh
@@ -3,7 +3,7 @@
 if [ $# -ne 1 ]
 then
     echo "ERROR: no arguments supplied."
-    echo "Usage: convert_githubrepo.sh *repo*"
+    echo "Usage: convert_githubrepo.sh [repository_folder]"
     exit 1
 fi
 
@@ -23,33 +23,19 @@ git fetch upstream
 git merge upstream/master
 git pull --recurse-submodules && git submodule update
 
-# Branch
+# Create branch and push
 git branch $current_date
 git checkout $current_date
 git push origin $current_date
 
-# Convert
-echo "Converting strings"
-if [ "$product_name" == "fxa-content-server-l10n" ]
-then
-	/usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o locale/sr_Latn/LC_MESSAGES/client.po locale/sr/LC_MESSAGES/client.po
-	/usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o locale/sr_Latn/LC_MESSAGES/server.po locale/sr/LC_MESSAGES/server.po
-	git add locale/sr_Latn/LC_MESSAGES/client.po
-	git add locale/sr_Latn/LC_MESSAGES/server.po
-elif [ "$product_name" == "zippy" ]
-then
-	/usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o locale/sr_LATN/LC_MESSAGES/messages.po locale/sr/LC_MESSAGES/messages.po
-	git add locale/sr_Latn/LC_MESSAGES/messages.po
-elif [ "$product_name" == "olympia" ] || [ "$product_name" == "zamboni" ]
-then
-	/usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o locale/sr_Latn/LC_MESSAGES/messages.po locale/sr/LC_MESSAGES/messages.po
-	/usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o locale/sr_Latn/LC_MESSAGES/javascript.po locale/sr/LC_MESSAGES/javascript.po
-	git add locale/sr_Latn/LC_MESSAGES/messages.po
-	git add locale/sr_Latn/LC_MESSAGES/javascript.po
-else
-	/usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o locale/sr_Latn/LC_MESSAGES/messages.po locale/sr/LC_MESSAGES/messages.po
-	git add locale/sr_Latn/LC_MESSAGES/messages.po
-fi
+# Get a list of .po files available in sr/LC_MESSAGES
+files=(locale/sr/LC_MESSAGES/*.po)
+for file in ${files[@]}
+do
+    latin_file="${file/sr\//sr_Latn/}"
+    /usr/local/opt/icu4c/bin/uconv -x Serbian-Latin/BGN -o $latin_file $file
+    git add $latin_file
+done
 
 # Add change to git
 git commit -a -m "Script conversion from Serbian Cyrillic to Latin ($current_date)"
