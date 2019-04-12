560a9d53ee03f0d8be13c1c00c468cedf7e4bbd3
Francesco Lodolo (:flod) <flodolo@mozilla.com>
2019-04-12 20:51:24 +0200

    Update script

--- pontoon/unreviewed_suggestions_lifespan.py
+++ pontoon/unreviewed_suggestions_lifespan.py
@@ -17,7 +17,7 @@ END_DATE = '11/04/2019'   # DD/MM/YYYY
 
 
 # Script
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.db.models import Avg, F
 from django.utils.timezone import get_current_timezone
 from pontoon.base.models import Locale, Translation
@@ -30,21 +30,52 @@ end_date = tz.localize(datetime.strptime(END_DATE, '%d/%m/%Y'))
 output = []
 output.append('Locale,Average Unreviewed Suggestion Lifespan')
 
+def divide_timedelta(td, divisor):
+    total_seconds = (td.microseconds + (td.seconds + td.days * 24 * 3600) * 1e6) / 1e6
+    divided_seconds = total_seconds / float(divisor)
+    return timedelta(seconds=divided_seconds)
+
 for locale in locales:
+    # Translations submitted in Pontoon for given locale and timeframe
     translations = Translation.objects.filter(
+        user__isnull=False,
         locale=locale,
         date__gte=start_date,
         date__lte=end_date,
-    ).exclude(
-        user=F('approved_user'),
-    ).aggregate(
-        average_delta=Avg(
-            F('approved_date') - F('date')
-        )
     )
+    # Above translations that have been approved, but not self-approved
+    approved = (
+        translations
+        .filter(approved_date__isnull=False)
+        .exclude(user=F('approved_user'))
+    )
+    # Above translations that have been rejected, but not self-rejected
+    rejected = (
+        translations
+        .filter(rejected_date__isnull=False)
+        .exclude(user=F('rejected_user'))
+    )
+    approved_delta = (
+        approved.aggregate(
+            average_delta=Avg(
+                F('approved_date') - F('date')
+            )
+        )
+    )['average_delta']
+    rejected_delta = (
+        rejected.aggregate(
+            average_delta=Avg(
+                F('rejected_date') - F('date')
+            )
+        )
+    )['average_delta']
+    try:
+        combined_delta = divide_timedelta(approved_delta + rejected_delta, 2)
+    except TypeError:
+        combined_delta = 0
     output.append('{},{}'.format(
         locale.code,
-        str(translations['average_delta']).replace(',', ''),
+        str(combined_delta).replace(',', ''),
     ))
 
 print('\n'.join(output))
