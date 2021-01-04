6176357ac06e262ad393f934bcd0ffd9b4c9f272
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2021-01-04 11:01:52 +0100

    Update hg locales script

--- mozilla_l10n/clone_hgmo/update_locales.py
+++ mozilla_l10n/clone_hgmo/update_locales.py
@@ -9,15 +9,10 @@ def query_hg(locales, url):
     try:
         response = urllib2.urlopen(url)
         json_data = json.load(response)
-        # hg.mozilla.org has 'entries', bitbucket 'values'
-        key_name = 'values' if 'values' in json_data else 'entries'
-        for repository in json_data[key_name]:
-            # Ignore x-testing in mozilla.org
-            if repository == 'x-testing':
-                continue
-            locales.append(repository['name'])
-        if 'next' in json_data:
-            query_hg(locales, json_data['next'])
+        for repository in json_data["entries"]:
+            locales.append(repository["name"])
+        if "next" in json_data:
+            query_hg(locales, json_data["next"])
         locales.sort()
     except Exception as e:
         print(e)
@@ -28,19 +23,18 @@ def main():
 
     locales_hgmo = []
     try:
-        url = 'https://hg.mozilla.org/l10n-central/?style=json'
-        print('Reading list of l10n-central repositories')
+        url = "https://hg.mozilla.org/l10n-central/?style=json"
+        print("Reading list of l10n-central repositories")
         query_hg(locales_hgmo, url)
-        # Ignore x-testing
-        locales_hgmo.remove('x-testing')
 
-        file_name = os.path.join(script_folder, 'locales_hgmo.txt')
-        with open(file_name, 'w') as f:
+        file_name = os.path.join(script_folder, "locales_hgmo.txt")
+        with open(file_name, "w") as f:
             locales_hgmo.sort()
             for locale in locales_hgmo:
-                f.write('{}\n'.format(locale))
+                f.write("{}\n".format(locale))
     except Exception as e:
         print(e)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
