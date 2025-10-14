8b1f1a737bba4d089d4421ec4408dc7e8616394c
Francesco Lodolo <flod@lodolo.net>
2025-10-14 07:54:08 +0200

    Remove unnecessary repos from macOS sync

--- mac/sync_moz_mac.sh
+++ mac/sync_moz_mac.sh
@@ -29,9 +29,6 @@ git_repositories=(
     "https://github.com/mozilla-firefox/firefox"
     "https://github.com/mozilla-l10n/firefox-l10n-source"
     "https://github.com/mozilla-l10n/firefox-l10n"
-    "https://github.com/mozilla/compare-locales"
-    "https://github.com/mozilla/fluent-migrate"
-    "https://github.com/projectfluent/python-fluent"
     "https://github.com/flodolo/firefox_l10n_checks"
     "https://github.com/flodolo/mozpm_stats"
 )
@@ -39,18 +36,12 @@ git_folder_names=(
     "mozilla-firefox"
     "firefox-quarantine"
     "firefox-l10n"
-    "compare-locales"
-    "fluent-migrate"
-    "python-fluent"
     "firefox_l10n_checks"
     "mozpm_stats"
 )
 git_branch=(
     "main"
     "update"
-    "main"
-    "release"
-    "main"
     "main"
     "main"
     "main"
